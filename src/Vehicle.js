f384bff307246780e2631dc4fdcb037fd12d3d55
David <Rigby335@hotmail.com>
2022-06-09 15:30:35 -0700

    Yegads! test

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,6 +1,6 @@
 export default function Vehicle({ vehicle }) {
   return <div className="vehicle">
-    {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
+    {/* the Vehicle component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {/* if the vehicle props is a car, render a '🚗' emoji */}
     {vehicle === 'car' && '🚗'}
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
--- src/VehicleList.js
+++ src/VehicleList.js
@@ -6,7 +6,7 @@ export default function VehicleList({ vehicles }) {
   return (
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
-      {/* map through the vehicles array in props */}
+      {/* map through the vehic array in props */}
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
       {
         vehicles.map((vehicle, i) => <Vehicle vehicle={vehicle} key={vehicle + i} />)
